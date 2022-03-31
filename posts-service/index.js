6d9b882666b4dd1475323edaf58913dddf0f7356
Ardrit Krasniqi <ardritdev@gmail.com>
2022-03-31 13:54:02 +0200

    added the post creation and fetching

--- posts-service/index.js
+++ posts-service/index.js
@@ -2,11 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.get('/posts', (req, res) => {
+const { randomBytes } = require('crypto');
 
-})
+const posts = {};
+
+// routes for fetching and saving posts
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
 
 
 app.post('/posts', (req, res) => {
-    
-})
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {
+        id,
+        title
+    };
+});
+
+// listen on certain port
+app.listen(4000, () => {
+    console.log("Listening on port: 4000")
+});
