1c313742ddeb7190c32030fd485fdfa1ba3783f7
Ardrit Krasniqi <ardritdev@gmail.com>
2022-04-03 00:06:14 +0200

    added the saving and fetching feature on comments

--- comments-service/index.js
+++ comments-service/index.js
@@ -1,3 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 
 const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = [];
+
+app.get('/posts/:id/comments', (req, res) => {
+
+    res.status(200).send(commentsByPostId[req.params.id] || []);
+});
+
+
+app.post('/posts/:id/comments', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const text = req.body.text;
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: id, text: text });
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(200).send(comments);
+});
+
+
+app.listen(4001, () => {
+    console.log("Listening on port: 4001")
+});
+
+
+
