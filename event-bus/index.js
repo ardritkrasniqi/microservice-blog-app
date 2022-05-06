65641b5db1c62379afbbc3a326dfe541fd60decc
Ardrit Krasniqi <ardritdev@gmail.com>
2022-05-06 23:07:37 +0200

    bugfixes on event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -20,17 +20,14 @@ app.post('/events', async (req, res) => {
 
 
     // send the event to different services
-    const posts = axios.post('http://posts-clusterip-srv:4000/events', event); // posts service
-    const comments = axios.post('http://localhost:4001/events', event).catch(function (error){
-        console.log(error);
-    }); // comments service
-    const event_buss = axios.post('http://localhost:4002/events', event); // event buss service
-    const moderation = axios.post('http://localhost:4006/events', event); // comment moderation service
-
-    Promise.all([posts, comments, event_buss, moderation])
-        .catch(function (error) {
-            console.log(error);
-        });
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch(function (error){
+        console.log(`The error is: ${error}`);
+    }); // posts service
+    // const comments = axios.post('http://localhost:4001/events', event).catch(function (error){
+    //     console.log(error);
+    // }); // comments service
+    // const event_buss = axios.post('http://localhost:4002/events', event); // event buss service
+    // const moderation = axios.post('http://localhost:4006/events', event); // comment moderation service
 
     res.send({ message: "success", status: "true" });
 });
@@ -42,5 +39,6 @@ app.get('/events', (req, res) => {
 })
 
 app.listen(port, () => {
+    console.log('this is the newest version!');
     console.log(`app listening on port ${port}`)
 });
